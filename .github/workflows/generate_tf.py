0db9343141c82ddaf942fa951894359631e4547d
Sudhanshu Jha <sudhanshu_jha@crgl-thirdparty.com>
2024-08-22 16:28:54 -0500

    change

--- .github/workflows/generate_tf.py
+++ .github/workflows/generate_tf.py
@@ -21,41 +21,65 @@ try:
 
     # Generate the Terraform configuration
     tf_config = f"""
+    terraform {{
+      required_providers {{
+        okta = {{
+          source  = "okta/okta"
+          version = "3.41.0"
+        }}
+      }}
+    }}
+
     provider "okta" {{
       org_name   = "dev-176298"
       base_url   = "okta.com"
       api_token  = "{okta_api_token}"
     }}
 
-    resource "okta_app_oauth" "example_app" {{
+    resource "okta_app_oauth" "dxp_app" {{
       label             = "{config['label']}"
       type              = "{config['type']}"
       grant_types       = ["implicit", "authorization_code"]
       response_types    = {config['response_types']}
       redirect_uris     = {config['redirect_uris']}
       post_logout_redirect_uris = {config.get('post_logout_redirect_uris', [])}
-      token_endpoint_auth_method = "{config['token_endpoint_auth_method']}"
+      client_uri        = "{config.get('client_uri', '')}"
+      tos_uri           = "{config.get('tos_uri', '')}"
+      policy_uri        = "{config.get('policy_uri', '')}"
     }}
 
     resource "okta_group" "internal_users" {{
-      name = "{config['groups'][0]}"
+      name        = "{config['groups'][0]}"
+      description = "Internal users group for {config['app_name']} app"
     }}
 
     resource "okta_group" "external_users" {{
-      name = "{config['groups'][1]}"
+      name        = "{config['groups'][1]}"
+      description = "External users group for {config['app_name']} app"
     }}
 
-    resource "okta_group" "deactive_users" {{
-      name = "{config['groups'][2]}"
+    resource "okta_group" "deactivated_users" {{
+      name        = "{config['groups'][2]}"
+      description = "Deactivated users group for {config['app_name']} app"
     }}
 
-    resource "okta_app_group_assignments" "example_app_groups" {{
-      app_id = okta_app_oauth.example_app.id
-      group_ids = [
-        okta_group.internal_users.id,
-        okta_group.external_users.id,
-        okta_group.deactive_users.id
-      ]
+    resource "okta_app_group_assignments" "dxp_app_assignments" {{
+      app_id = okta_app_oauth.dxp_app.id
+      group {{
+        id = okta_group.internal_users.id
+      }}
+      group {{
+        id = okta_group.external_users.id
+      }}
+      group {{
+        id = okta_group.deactivated_users.id
+      }}
+    }}
+
+    output "dxp_app_client_secret" {{
+      value       = okta_app_oauth.dxp_app.client_secret
+      sensitive   = true
+      description = "The client secret for the {config['app_name']} App"
     }}
     """
 
@@ -64,5 +88,9 @@ try:
         file.write(tf_config)
 
 except Exception as e:
+    print('lalalalalalalalaalalalalalalalalalalalalalalala')
     print(f"Error: {e}")
+    print("Traceback:")
+    traceback.print_exc()
+    sys.exit(1)
     exit(1)
